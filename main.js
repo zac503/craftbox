dbcc0ca76ad464be56e3078bd69066de963de997
Zach Phillips <zac503@gmail.com>
2015-09-29 07:03:35 -0400

    Added rudimentary resource collection for testing

--- main.js
+++ main.js
@@ -11,7 +11,14 @@ var workers = 0;
 
 var resourceList = new Array();
 var forestMeter = document.getElementById("forest-meter");
-var forest = {name:"Forest",timer:40,current:0,lvl:1,assignedWorkers:0,running:0,meter:forestMeter};
+var forest = {name:"Forest",timer:10,current:0,lvl:1,assignedWorkers:0,running:0,meter:forestMeter};
+var forestDrops = new Array();
+
+var fDrop1 ={name:"Wood"};
+
+//Inventory
+var resourceInv = new Array();
+
 
 
 function init() {
@@ -20,6 +27,7 @@ function init() {
 	menuitems = document.getElementById("menuitems");
 	
 	resourceList[0] = forest;
+	forestDrops[0] = fDrop1;
 	
 	console.log(forest.running);
 
@@ -72,6 +80,19 @@ function showTab(tabName) {
 	 
  }
  
+ function updateResourceList() {
+	 
+	 var rBlock = document.getElementById("resource-list");
+	 
+	 for(i=0;i<resourceInv.length; i++)
+	 {
+		 
+		 rBlock.innerHTML = resourceInv[i].name + ": " + resourceInv[i].qty;
+		 
+	 }
+	 
+ }
+ 
  
  
  
@@ -95,8 +116,19 @@ function showTab(tabName) {
 			 resourceList[i].current = 0;
 			 console.log("reset " + resourceList[i].name);
 			 document.getElementById("forest-meter").style.width = "0%";
+			 console.log("Found " + forestDrops[0].name);
+			 resourceInv.push(forestDrops[0]);
+			 if(resourceInv[0].qty == null)
+			 {
+				 resourceInv[0].qty = 0;
+				 
+			 }
+			 resourceInv[0].qty = resourceInv[0].qty + 1;
+			 console.log(resourceInv[0].qty);
 			 
 		 }
+		 
+		 updateResourceList();
 		 
 	 }
 	 
