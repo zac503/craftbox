38ad7871aa38a7bab1862fd9237a12a7ad594846
Zach Phillips <zac503@gmail.com>
2015-10-01 08:00:46 -0400

    testing jquery

--- scripts/jsontest.js
+++ scripts/jsontest.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 
 $.getJSON( "test.json", function( data ) {
   var items = [];
+  var parse = parseJSON(data);
+  console.log(parse);
   $.each( data, function( key, val ) {
     items.push( "<li id='" + key + "'>" + val + "</li>" );
   });
