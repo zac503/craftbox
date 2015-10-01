7ccb4883617f4cd728ed8005302a6f664e34cefc
Zach Phillips <zac503@gmail.com>
2015-09-30 22:26:39 -0400

    Test Jquery

---
+++ scripts/jsontest.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+
+$.getJSON( "test.json", function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  });
+ 
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
+
+});
