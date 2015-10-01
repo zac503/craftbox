6c4a176a7fdd9c069757b761a405795acf5fe9e5
Zach Phillips <zac503@gmail.com>
2015-09-30 22:56:28 -0400

    testing jquery

--- scripts/jsontest.js
+++ scripts/jsontest.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
-var obj;
+
 $.getJSON('test.json', function(data) {
-  obj = jQuery.parseJSON(data);
+  var obj = jQuery.parseJSON(data);
   $('#json').html(obj.one);
 });
 
